44759f0f1abe993311485833421bfa8f7bd1d76a
ArekuseyHentai <black.akl@yandex.ru>
2024-08-25 19:00:53 +0300

    Global Update v2

---
+++ src/js/scrypt.js
@@ -0,0 +1,5 @@
+$('.hidden-images').hover(function(){
+    $('.hidden-images').css('opacity:','0.8')
+    $('.hidden-images').css('transform:','scale(0.9)')
+    $('.image-holder>img').css('transform:','scale(0.9)')
+})
