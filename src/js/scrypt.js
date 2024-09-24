10d322b5a72e5aef6cbe6c269b6ef50881f81d77
ArekuseyHentai <black.akl@yandex.ru>
2024-09-24 16:24:32 +0300

    123
    123

--- src/js/scrypt.js
+++
@@ -1,5 +0,0 @@
-$('.hidden-images').hover(function(){
-    $('.hidden-images').css('opacity:','0.8')
-    $('.hidden-images').css('transform:','scale(0.9)')
-    $('.image-holder>img').css('transform:','scale(0.9)')
-})
